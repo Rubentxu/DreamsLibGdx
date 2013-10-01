26d29870f847b345e35860023bceb2770d515448
rubentxu <rubentxu74@gmail.com>
2013-10-01 18:10:14 +0200

    nuevos cambios

---
+++ core/src/main/java/com/rubentxu/juegos/core/modelo/EnemyObject.java
@@ -0,0 +1,11 @@
+package com.rubentxu.juegos.core.modelo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rubentxu
+ * Date: 1/10/13
+ * Time: 10:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class EnemyObject {
+}
