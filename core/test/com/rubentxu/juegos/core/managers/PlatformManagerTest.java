83417909e91ea7c0518830f3cd0750dd12623042
rubentxu <rubentxu74@gmail.com>
2013-11-27 18:22:14 +0100

    test platformManager

--- core/test/com/rubentxu/juegos/core/managers/PlatformManagerTest.java
+++ core/test/com/rubentxu/juegos/core/managers/PlatformManagerTest.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.utils.GdxNativesLoader;
 import com.rubentxu.juegos.core.controladores.WorldController;
 import com.rubentxu.juegos.core.modelo.Box2DPhysicsObject;
 import com.rubentxu.juegos.core.modelo.MovingPlatform;
+import com.rubentxu.juegos.core.modelo.Rubentxu;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -32,6 +33,7 @@ public class PlatformManagerTest {
     private Fixture fixMock;
     private MovingPlatform m1;
     private MovingPlatform m2;
+    private static Rubentxu r;
 
 
     private class MyContact extends Contact {
@@ -64,23 +66,50 @@ public class PlatformManagerTest {
     public void setup(){
         Body body1 = createBox(BodyDef.BodyType.KinematicBody, 4, 0.5f, 1);
         Body body2 = createBox(BodyDef.BodyType.KinematicBody, 4, 1, 1);
+        contact = createNiceMock(Contact.class);
+        fixMock= createNiceMock(Fixture.class);
         m1= new MovingPlatform("M1", PLATAFORMAS_MOVILES, body1,
                 68,5,64,9,4);
 
         m2= new MovingPlatform("M2", PLATAFORMAS_MOVILES,body2,
                 78,4,80,9,4 );
+        r = new Rubentxu(physic);
+        r.createRubenxu(0,0,0.7f,1.8f);
     }
 
-    /* Test sobre el metodo applyImpulses */
+
     @Test
     public void testUpdateMovingPlatform() {
-        platformManager.updateMovingPlatform(m1,1);
-
+        float delta=1;
+        float distance = m1.getDistance();
+        platformManager.updateMovingPlatform(m1,delta);
+        assertFalse(m1.getForward());
+        assertEquals(m1.getVelocity(), m1.getpVelocity());
+        assertEquals(distance + m1.getVelocity().len() * delta ,m1.getDistance(),0);
+        System.out.println("Distance1: "+ m1.getDistance());
     }
 
+    @Test
+    public void testUpdateMovingPlatformExceededMaxDist() {
+        float delta=2;
+        float distance = m1.getDistance();
+        platformManager.updateMovingPlatform(m1,delta);
+        assertTrue(m1.getForward());
+        assertTrue(m1.getDistance() == 0);
+        assertTrue(m1.getMaxDist() < distance + m1.getVelocity().len() * delta);
+        platformManager.updateMovingPlatform(m1,0.1f);
+        assertEquals(m1.getVelocity(), m1.getpVelocity().scl(-1f));
+        System.out.println("Distance2: "+ m1.getDistance());
+    }
 
-
-
+    @Test
+    public void testUpdateMovingPlatformDisabled() {
+        float delta=2;
+        m1.enabled=false;
+        platformManager.updateMovingPlatform(m1,delta);
+        assertEquals(m1.getVelocity(), new Vector2(0,0));
+        System.out.println("Distance3: "+ m1.getDistance());
+    }
 
 
 
